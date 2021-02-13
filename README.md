90bd7cce7a912af7df39c5ed92d2ded438269e72
xinacc1 <77516791+xinacc1@users.noreply.github.com>
2021-02-13 10:17:12 +1000

    With Google Analytics Tag
    And some of case study page completed with Bootstrap 4

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# UX_UI_HW_20
+# UX_UI_HW_18
---
+++ js/index.js
@@ -0,0 +1,33 @@
+$("#firstproject").hover(
+    function(){$(this).animate({"width": "425px", "height": "412px", "left": "-10px"},500);},
+    function(){$(this).animate({"width": "405px", "height": "392px", "left": "0px"},1000);},
+);
+
+$("#secondproject").hover(
+    function(){$(this).animate({"width": "425px", "height": "412px", "left": "-10px"},500);},
+    function(){$(this).animate({"width": "405px", "height": "392px", "left": "0px"},1000);},
+);
+
+$("#thirdproject").hover(
+    function(){$(this).animate({"width": "425px", "height": "412px", "left": "-10px"},500);},
+    function(){$(this).animate({"width": "405px", "height": "392px", "left": "0px"},1000);},
+);
+
+$("#fourthproject").hover(
+    function(){$(this).animate({"width": "425px", "height": "412px", "left": "-10px"},500);},
+    function(){$(this).animate({"width": "405px", "height": "392px", "left": "0px"},1000);},
+);
+
+$("#fifthproject").hover(
+    function(){$(this).animate({"width": "425px", "height": "412px", "left": "-10px"},500);},
+    function(){$(this).animate({"width": "405px", "height": "392px", "left": "0px"},1000);},
+);
+
+$("#sixthproject").hover(
+    function(){$(this).animate({"width": "425px", "height": "412px", "left": "-10px"},500);},
+    function(){$(this).animate({"width": "405px", "height": "392px", "left": "0px"},1000);},
+);
+ 
+ 
+
+ 
